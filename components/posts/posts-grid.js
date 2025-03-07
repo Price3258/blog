df81479987d4755f7483dd9fe07bd487072c956d
김원태 <rladnjsxo123@naver.com>
2025-03-07 20:43:05 +0900

    363. 더미 게시물 데이터 렌더링하기

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -7,7 +7,7 @@ export default function PostsGrid(props) {
   return (
     <ul className={classes.grid}>
       {posts.map((post) => (
-        <PostItem key={post.id} item={post} />
+        <PostItem key={post.slug} post={post} />
       ))}
     </ul>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,43 @@
+import FeaturedPosts from "@/components/home-page/featured-posts";
 import Hero from "@/components/home-page/hero";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "Nextjs is a the React frameworks",
+    date: "2023-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Nextjs is a the React frameworks for production - it makes building fullstack React app",
+    date: "2023-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "Nextjs is a the React frameworks",
+    date: "2023-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "Nextjs is a the React frameworks",
+    date: "2023-02-10",
+  },
+];
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
   );
 }
