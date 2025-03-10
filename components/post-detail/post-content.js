589fe423857d84d722f3b0420f287aa445599ed1
김원태 <rladnjsxo123@naver.com>
2025-03-10 17:41:40 +0900

    372. “Next Image” 컴포넌트를 통해 (마크다운으로부터) 이미지 렌더링하기

--- components/post-detail/post-content.js
+++ components/post-detail/post-content.js
@@ -1,6 +1,7 @@
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import ReactMarkDown from "react-markdown";
+import Image from "next/image";
 
 const DUMMY_POST = {
   slug: "getting-started-with-nextjs",
@@ -13,10 +14,43 @@ const DUMMY_POST = {
 export default function PostContent(props) {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkDown>{post.content}</ReactMarkDown>
+      <ReactMarkDown components={customRenderers}>{post.content}</ReactMarkDown>
     </article>
   );
 }
