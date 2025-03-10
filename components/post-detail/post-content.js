67d3cf7f179c909ab6bb160fd54b5170de3718d2
김원태 <rladnjsxo123@naver.com>
2025-03-10 16:43:35 +0900

    366. 마크다운을 JSX로 렌더링하기

--- components/post-detail/post-content.js
+++ components/post-detail/post-content.js
@@ -1,12 +1,13 @@
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
+import ReactMarkDown from "react-markdown";
 
 const DUMMY_POST = {
   slug: "getting-started-with-nextjs",
   title: "Getting started with nextjs",
   image: "getting-started-nextjs.png",
   date: "2023-02-10",
-  content: "# This is a fisrt post",
+  content: "# This is a first post",
 };
 
 export default function PostContent() {
@@ -14,7 +15,7 @@ export default function PostContent() {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkDown>{DUMMY_POST.content}</ReactMarkDown>
     </article>
   );
 }
