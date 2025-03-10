bb1f982d808494d0be9b2b5deb82d8dfca1c8570
김원태 <rladnjsxo123@naver.com>
2025-03-10 19:09:57 +0900

    379. “head” 데이터 추가하기

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import FeaturedPosts from "@/components/home-page/featured-posts";
 import Hero from "@/components/home-page/hero";
 import { getFeaturedPosts } from "@/lib/posts-util";
+import Head from "next/head";
 
 export default function HomePage(props) {
   return (
     <>
+      <Head>
+        <title>Wontae' Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development and my life."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,17 @@
 import AllPosts from "@/components/posts/all-posts";
 import { getAllPosts } from "@/lib/posts-util";
+import Head from "next/head";
 
 export default function AllPostPage(props) {
-  return <AllPosts posts={props.posts} />;
+  return (
+    <>
+      <Head>
+        <title>All Posts</title>
+        <meta name="description" content="A list of all posts!" />
+      </Head>
+      <AllPosts posts={props.posts} />
+    </>
+  );
 }
 
 export function getStaticProps() {
